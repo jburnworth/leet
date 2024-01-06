66f8edbde337e5ceb8d3897e6fc0849ddd51ded7
James Burnworth <j.j.burnworth@gmail.com>
2024-01-06 15:58:54 -0700

    Commit a base working file to reset to.

--- working_file.cpp
+++ working_file.cpp
@@ -4,23 +4,9 @@
 
 using namespace std;
 
-bool isEven(const int& a, const int& b) {
-    return (a % 2) < (b % 2);
-}
-
-vector<int> sortArrayByParity(vector<int>& nums) {
-    sort(nums.begin(), nums.end(), isEven);
-    return nums;
-}
-
 int main()
 {
-    //vector<int> v = {3, 4, 5, 7, 5, 2, 1};
-    vector<int> v = {3363,4833,290,3381,4227,1711,1253,2984,2212,874,2358,2049,2846,2543,1557,1786,4189,1254,2803,62,3708,1679,228,1404,1200,4766,1761,1439,1796,4735,3169,3106,3578,1940,2072,3254,7,961,1672,1197,3187,1893,4377,2841,2072,2011,3509,2091,3311,233};
-    vector<int> missing = sortArrayByParity(v);
+    cout << "Hello" << endl;
 
-    cout << "Missing" << endl;
-    for (int m: missing) {
-        cout << m << endl;
-    }
+    return 0;
 }
